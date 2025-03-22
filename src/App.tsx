d73ee052b4591dbeabfc9d90f5b1454f21905a0c
tmdtmddl <tmddlqkqh@naver.com>
2025-03-22 20:34:50 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,7 @@
-import { useState } from "react";
+import React from "react";
 
 const App = () => {
-  const [texts, setTexts] = useState<string[]>([]);
-  const [text, setText] = useState("");
-
-  return (
-    <div>
-      <form
-        action=""
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (text.length !== 0) {
-            setTexts((prev) => [...prev, text]);
-            return setText("");
-          }
-        }}
-      >
-        <label htmlFor="todo">todo</label>
-        <input
-          id="todo"
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button>추가</button>
-      </form>
-      <ul>
-        {texts.map((text, index) => {
-          return (
-            <li key={index}>
-              <p>
-                {index + 1}.{text}
-              </p>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
