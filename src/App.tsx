f43b0e10a475b56b299cd81a274343597244e673
tmdtmddl <tmddlqkqh@naver.com>
2026-01-06 10:46:13 +0900

    teststst

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 const App = () => {
-  return <div>App</div>;
+  const [yes, setYes] = useState(false);
+  const swit = () => {
+    setYes(!yes);
+  };
+
+  return (
+    <div>
+      <p
+        className={twMerge(
+          "font-bold cursor-pointer border-2",
+          yes ? "text-black" : "text-red-500"
+        )}
+        onClick={swit}
+      >
+        Test
+      </p>
+    </div>
+  );
 };
 
 export default App;
+
+// onClick={() => setSelectedPost(null)}
