6d05c5aceb28204e64b0cfcd408f563914c0e9d1
tmdtmddl <tmddlqkqh@naver.com>
2026-01-18 18:05:13 +0900

    수정로직 완료

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Dispatch, SetStateAction } from "react";
+import TodoForm from "./Todoform";
 
 interface ItemProps {
   todos: string[];
@@ -8,7 +9,13 @@ interface ItemProps {
   payload: string; //새로 받으면 그걸로 교체
 }
 
-const TodoItem = ({ index, payload, setTodos }: ItemProps) => {
+const TodoItem = ({ index, payload, setTodos, todos }: ItemProps) => {
+  const [todoEdit, setTodoEdit] = useState(false);
+
+  const todoSwtich = () => {
+    setTodoEdit((prev) => !prev);
+  };
+
   const todoDelete = () => {
     if (confirm("삭제하시겠습니까?")) {
       setTodos((prev) => prev.filter((item) => item !== payload)); //삭제 버튼을 누른 그 TodoItem이 들고 있는 payload를 기준으로,todos(prev)를 전부 돌면서(item 하나씩) payload와 같은 값만 빼고 남긴다.
@@ -19,18 +26,36 @@ const TodoItem = ({ index, payload, setTodos }: ItemProps) => {
   };
 
   return (
-    <li className="p-3 border-2 hover:border-sky-700 rounded-xl my-2 flex justify-between items-center gap-4 max-w-lg">
-      <div className="flex-1 warp-break-words whitespace-normal">
-        {index + 1}. {payload}
-      </div>
-      <div>
-        <button
-          className="  hover:text-sky-600 text-red-400 font-bold cursor-pointer"
-          onClick={todoDelete}
-        >
-          삭제
-        </button>
-      </div>
+    <li className="p-3 border-2 hover:border-sky-700 rounded-xl my-2 flex justify-between items-center gap-4 max-w-xl min-w-xl hover:bg-sky-50">
+      {todoEdit ? (
+        <TodoForm
+          setTodos={setTodos}
+          todos={todos}
+          payload={payload}
+          todoEdit={todoEdit}
+          onCancel={todoSwtich} //TODO: 취소용
+        />
+      ) : (
+        <>
+          <div className="flex-1 warp-break-words whitespace-normal">
+            {index + 1}. {payload}
+          </div>
+          <div className="flex gap-2">
+            <button
+              className="  hover:text-sky-600 text-red-400 font-bold cursor-pointer"
+              onClick={todoDelete}
+            >
+              삭제
+            </button>
+            <button
+              className="  hover:text-sky-600 text-blue-400 font-bold cursor-pointer"
+              onClick={todoSwtich}
+            >
+              수정
+            </button>
+          </div>
+        </>
+      )}
     </li>
   );
 };
--- src/Todoform.tsx
+++ src/Todoform.tsx
@@ -4,26 +4,56 @@ import { Dispatch, SetStateAction } from "react";
 interface FormProps {
   todos: string[];
   setTodos: Dispatch<SetStateAction<string[]>>;
+  payload?: string;
+  todoEdit?: boolean;
+  onCancel?: () => void;
 }
 
-const TodoForm = ({ setTodos, todos }: FormProps) => {
-  const [text, setText] = useState("");
+const TodoForm = ({
+  setTodos,
+  todos,
+  payload,
+  todoEdit,
+  onCancel,
+}: FormProps) => {
+  const [todo, setTodo] = useState(payload ?? "");
 
   const ref = useRef<HTMLInputElement>(null);
 
   const onChange = (e) => {
-    setText(() => e.target.value);
+    setTodo(() => e.target.value);
   };
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (text.length === 0) {
+    if (todo.length === 0) {
       return alert("할일을 입력해주세요!");
     }
 
-    setTodos((prev) => [...prev, text]); //  배열에 추가
-    setText(""); // 입력창 비우기
+    setTodos((prev) => {
+      let copy = [...prev]; //기존 할 일 목록을 그대로 복사 (복사해서 고치고 원본은 ㄴㄴ(prev=["a","b","c"], copy=["a","b","c"]))
+
+      if (todoEdit) {
+        const index = todos.findIndex((p) => p === payload); //고칠 대상이 몇 번째에 있는지 찾기 todos뒤지면서 payload랑 같은 것 찾기 찾으면 그 위치 index줌 (ex: index=1)
+        if (index >= 0) {
+          //index === -1 → 못 찾음 , index >= 0 → 찾음
+          copy[index] = todo; //!그 자리에 새 내용으로 교체 (copy=복사본,index는 위치에 있는 내용을 todo로 바꿔라)
+        }
+      } else {
+        copy.unshift(todo); //새 할 일을 맨 앞에 넣기
+      }
+
+      return copy; //! 완성된 새 목록을 돌려줌 (이렇게 바꾼 결과를 이제 todos로 써주세요)
+    });
+
+    alert(todoEdit ? "수정되었습니다." : "추가되었습니다.");
+    if (todoEdit && onCancel) {
+      //수정하고 수정을 취소할경우에만 다시 todoEdit을 false로 바꿔라
+      onCancel();
+    }
+
+    setTodo(""); // 입력창 비우기
   };
 
   useEffect(() => {
@@ -36,7 +66,7 @@ const TodoForm = ({ setTodos, todos }: FormProps) => {
         type="text"
         className=" border-2 border-r-0  border-sky-500 rounded-l-2xl cursor-pointer p-2   outline-0 min-w-lg"
         placeholder="메모를 입력하세요!"
-        value={text}
+        value={todo}
         onChange={onChange}
         ref={ref}
       />
@@ -45,8 +75,17 @@ const TodoForm = ({ setTodos, todos }: FormProps) => {
           className=" border-2 border-l-0 border-sky-500  p-2 rounded-r-2xl cursor-pointer"
           type="submit"
         >
-          추가
+          {todoEdit ? "수정" : "추가"}
         </button>
+        {todoEdit && (
+          <button
+            className=" ml-2 border-2 border-sky-500 p-2 rounded-2xl cursor-pointer"
+            type="button"
+            onClick={onCancel}
+          >
+            취소
+          </button>
+        )}
       </div>
     </form>
   );
